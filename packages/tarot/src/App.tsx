b9ed3c4ef97f2f12c79736406bcb280572823659
trung <phamxuantrung02122003@gmail.com>
2025-07-24 22:54:31 +0700

    feat: - Integrate tailwind - Integrate material ui

--- packages/tarot/src/App.tsx
+++ packages/tarot/src/App.tsx
@@ -1,9 +1,41 @@
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
 function App() {
-  return (
+  const card = (
     <>
-      <h1>Flash Card Fav</h1>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+          Word of the Day
+        </Typography>
+        <Typography variant='h5' component='div'>
+          New Card
+        </Typography>
+        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
+          adjective
+        </Typography>
+        <Typography variant='body2'>
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small'>Learn More</Button>
+      </CardActions>
     </>
   );
-}  
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <Card>{card}</Card>
+      </div>
+    </>
+  );
+}
 
 export default App;
--- packages/tarot/src/main.tsx
+++ packages/tarot/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles='@layer theme, base, mui, components, utilities;' />
+      <App />
+    </StyledEngineProvider>
   </StrictMode>
 );
