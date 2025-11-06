42f14ced9660c33f7161704db7b94b27a357b371
aeron <sonpx130998@gmail.com>
2025-11-06 13:46:39 +0700

    feat(snake): use token from design tokens

--- packages/snake-game/vite.config.ts
+++ packages/snake-game/vite.config.ts
@@ -1,29 +1,34 @@
-import federation from '@originjs/vite-plugin-federation'
-import path from "path"
-import baseConfig from '../../vite.config.base'
-import { mergeConfig } from 'vite';
+import federation from "@originjs/vite-plugin-federation";
+import path from "path";
+import baseConfig from "../../vite.config.base";
+import { mergeConfig } from "vite";
 
 // https://vite.dev/config/
 export default mergeConfig(baseConfig, {
   server: {
     port: 5006,
-    cors: true    
+    cors: true,
   },
   preview: {
     port: 5006,
   },
   plugins: [
     federation({
-        name: 'snake-game',
-        filename: 'remoteEntry.js',
-        exposes: {
-          './app': './src/App.tsx',
-        },
-        shared: ['react', 'react-dom', 'react-router-dom', 'tailwindcss']
-    })],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+      name: "snake-game",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./app": "./src/App.tsx",
       },
+      shared: ["react", "react-dom", "react-router-dom", "tailwindcss", "@microservice-research/event-bus"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@microservice-research/design-tokens": path.resolve(
+        __dirname,
+        "../design-tokens/src"
+      ),
     },
-})
+  },
+});
